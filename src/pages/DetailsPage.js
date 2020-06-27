7ccc787857f6ea58e1ba1f465a8cb6a1b15a649d
Jawad Halawa <60158514+88jawad88@users.noreply.github.com>
2020-06-27 20:56:50 +0300

    Update DetailsPage.js

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -6,7 +6,6 @@ import Header from "../components/Header.js";
 class DetailsPage extends React.Component {
   render() {
     const bookId = this.props.match.params.id;
-    console.log(bookId,"bookId")
     return (
       <>
         <Header />

c861dac3ea442f97d976b16864af783b736f5583
Jawad Halawa <60158514+88jawad88@users.noreply.github.com>
2020-06-27 20:58:27 +0300

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,9 +6,7 @@ class Book extends React.Component {
     return (
       <li className="books">
         <NavLink exact to={`./details/${this.props.book.id}`}>
-          {/* <a href={this.props.link} target="_blank"> */}
           <img src={this.props.book.volumeInfo.imageLinks.thumbnail} alt={this.props.book.volumeInfo.title} />
-          {/* </a> */}
         </NavLink>
       </li>
     );

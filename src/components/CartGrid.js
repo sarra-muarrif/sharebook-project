48487d6ec1d7a36500744427ee8fa7b3a3b93f6b
Jawad Halawa <60158514+88jawad88@users.noreply.github.com>
2020-07-03 15:55:35 +0300

    Update CartGrid.js

--- src/components/CartGrid.js
+++ src/components/CartGrid.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
-class CartGrid extends React.Component {
 
+class CartGrid extends React.Component {
   render() {
     const {handleDelete} = this.props
     return (

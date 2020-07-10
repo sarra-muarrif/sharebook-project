415192a2775d315ae33842a57c317d8506b5014f
Ali <alitramb8@gmail.com>
2020-07-10 16:21:56 +0300

    delete input count

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const AddToCartButton = (props) => {
-  const [count, SetCount] = useState(0);
+  const [count] = useState(0);
 
   return (
     <>
@@ -10,20 +10,7 @@ const AddToCartButton = (props) => {
         onClick={() => props.catchItem({ ...props.item, count })}
       >
         Add To Cart
-        <span className="input-count-details">
-          <input
-            className="input-count"
-            type="number"
-            name="count"
-            placeholder="0"
-            onChange={(e) => SetCount(e.target.value)}
-          />
-        </span>
       </button>
-      {/* exmple  */}
-      {/* <button onClick={(e) => SetCount(count + 1)}>
-        Click me
-      </button> */}
     </>
   );
 };

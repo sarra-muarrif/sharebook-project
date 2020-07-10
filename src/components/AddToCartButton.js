b0cb81e9a637659fc399531f341aa8d9758dd848
Ali <alitramb8@gmail.com>
2020-07-10 16:47:58 +0300

    delete input count

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 const AddToCartButton = (props) => {
-  
-
   return (
     <>
       <button
         className="add-to-cart"
-        onClick={() => props.catchItem({ ...props.item})}
+        onClick={() => props.catchItem({ ...props.item })}
       >
         Add To Cart
       </button>

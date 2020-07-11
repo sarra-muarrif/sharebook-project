44e69c47616ac7bbaf5363d795c5b1f6b73ad275
sarra-muarrif <saraalsara@hotmail.com>
2020-07-11 20:53:14 +0300

    passing the price in details-page component and book-grid component

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -6,18 +6,27 @@ import Header from "../components/Header.js";
 class DetailsPage extends React.Component {
   render() {
     const bookId = this.props.match.params.id;
+    const book = this.props.books.find((book) => book.id.includes(bookId));
+    //Filter Price
+    const filterPrice =
+      book &&
+      (book.saleInfo.saleability === "NOT_FOR_SALE" ||
+      book.saleInfo.saleability === "FREE"
+        ? 20
+        : book.saleInfo.retailPrice.amount);
     return (
       <>
         <Header />
         <Details
-          books={this.props.books}
-          bookid={bookId}
+          book={book}
           catchItem={this.props.catchItem}
+          filterPrice={filterPrice}
         />
         <BookGridDetails
           limit={4}
           books={this.props.books}
           catchItem={this.props.catchItem}
+          price={filterPrice}
         />
       </>
     );

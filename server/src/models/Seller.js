65b10ec591166097258e79525dd81d0981deeef3
jawad <jawad88halawa@gmail.com>
2020-08-06 22:59:33 +0300

    modifing seller for comments

--- server/src/models/Seller.js
+++ server/src/models/Seller.js
@@ -2,14 +2,14 @@
 // 1.import the mongoose library
 const { Schema, model } = require("mongoose");
 
-// 2.start defining your user schema
+// 2.start defining book schema
 const SellerBookSchema = new Schema({
   title: String,
   price: Number,
   type: String,
 });
 
-// 3.create  the user model
+// 3.create  the book model
 const SellerBook = model("seller", SellerBookSchema);
 
 // 4.export the module

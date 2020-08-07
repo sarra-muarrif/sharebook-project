a76264fdb114ddee5850576b54a1e59d4f6fb82b
sarra-muarrif <saraalsara@hotmail.com>
2020-08-07 06:46:46 +0300

    merage master

--- server/src/router/router.js
+++ server/src/router/router.js
@@ -4,52 +4,50 @@ const router = require("express").Router();
 const user = require("../models/users");
 const SellerBook = require("../models/Seller");
 
-  //git seller books
-  router.get("/seller", async (req, res) => {
-    try {
-      const sellerBooks = await SellerBook.find({});
-      //res.json(sellerBooks);
-      res.send(sellerBooks);
-    } catch (error) {
-      res.status(500).json({
-        Error: console.log(error),
-      });
-    }
-    //add new seller books
-    router.post("/seller", async (req, res) => {
-      const { title, price, type } = req.body;
-      const newSellerBook = new SellerBook({
-        title,
-        price,
-        type,
-      });
-      try {
-        await newSellerBook.save();
-        res.json(newSellerBook);
-        console.log(req.body);
-      } catch (error) {
-        res.status(500).json({
-          Error: error,
-        });
-      }
+//git seller books
+router.get("/seller", async (req, res) => {
+  try {
+    const sellerBooks = await SellerBook.find({});
+    res.send(sellerBooks);
+  } catch (error) {
+    res.status(500).json({
+      Error: console.log(error),
     });
-  });
-  //delete seller books
-  router.delete("/seller/:id", async (req, res) => {
-    const { id } = req.params;
+  }
+  //add new seller books
+  router.post("/seller", async (req, res) => {
+    const { title, price, type } = req.body;
+    const newSellerBook = new SellerBook({
+      title,
+      price,
+      type,
+    });
     try {
-      const deleteSellerBook = await SellerBook.deleteOne({
-        _id: id,
-      });
-      res.json(deleteSellerBook);
+      await newSellerBook.save();
+      res.json(newSellerBook);
+      console.log(req.body);
     } catch (error) {
       res.status(500).json({
         Error: error,
       });
     }
   });
+});
+//delete seller books
+router.delete("/seller/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteSellerBook = await SellerBook.deleteOne({
+      _id: id,
+    });
+    res.json(deleteSellerBook);
+  } catch (error) {
+    res.status(500).json({
+      Error: error,
+    });
+  }
+});
 
- // app.get("*", (req, res) => res.send("URL NOT FOUND"));
 
 //get all users
 router.get("/users", async (req, res) => {
@@ -92,4 +90,5 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.get("*", (res) => res.send("URL NOT FOUND"));
 module.exports = router;
